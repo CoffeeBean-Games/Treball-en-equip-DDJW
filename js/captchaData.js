9cdc96d9b7e0e280c96f2132c57d89f78e00d885
u6103687 <pautano2005@gmail.com>
2026-05-18 09:22:35 +0200

    Estructura del captcha feta (biblioteca/emmagatzematge), falta encara possar una carpeta de les imatges/imatges als assets, ara mateix el codi te noms generics rollo 1_1_real o 1_1_fake, un cop es pugin les imatges s'haura de possar també la ruta

---
+++ js/captchaData.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const captchaData = [
+    {
+        graella: [
+            [
+                { variants: [ {img: '0_0_real.png', fake: false}, {img: '0_0_fake.png', fake: true} ] },
+                { variants: [ {img: '0_1_real.png', fake: false}, {img: '0_1_fake.png', fake: true} ] },
+                { variants: [ {img: '0_2_real.png', fake: false}, {img: '0_2_fake.png', fake: true} ] }
+	    ],
+            [
+                { variants: [ {img: '1_0_real.png', fake: false}, {img: '1_0_fake.png', fake: true} ] },
+                { variants: [ {img: '1_1_real.png', fake: false}, {img: '1_1_fake.png', fake: true} ] },
+                { variants: [ {img: '1_2_real.png', fake: false}, {img: '1_2_fake.png', fake: true} ] }
+            ],
+            [
+                { variants: [ {img: '2_0_real.png', fake: false}, {img: '2_0_fake.png', fake: true} ] },
+                { variants: [ {img: '2_1_real.png', fake: false}, {img: '2_1_fake.png', fake: true} ] },
+                { variants: [ {img: '2_2_real.png', fake: false}, {img: '2_2_fake.png', fake: true} ] }
+            ]
+        ]
+    }
+];
