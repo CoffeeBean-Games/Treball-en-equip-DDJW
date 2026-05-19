f56f3d06fc1476ff9389e6a33d249bb91329685e
u6103681 <u6103681@campus.udg.edu>
2026-05-19 22:31:05 +0200

    Issue implementar menu de pausa tencada correctament

--- js/scenes/pauseScene.js
+++ js/scenes/pauseScene.js
@@ -1,1 +1,90 @@
+"use strict";
 
+class pauseScene extends Phaser.Scene {
+
+    constructor() {
+        super('pauseScene');
+    }
+
+    create() {
+        const cx = this.cameras.main.width / 2;
+        const cy = this.cameras.main.height / 2;
+
+        this.add.rectangle(cx, cy, this.cameras.main.width, this.cameras.main.height, 0x000000, 0.7);
+
+        this.add.rectangle(cx, cy, 360, 320, 0x16213e)
+            .setStrokeStyle(2, 0xe94560);
+
+        this.add.text(cx, cy - 110, 'PAUSA', {
+            fontSize: '32px',
+            fill: '#e94560',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        this.add.rectangle(cx, cy - 75, 280, 2, 0xe94560);
+
+        this.botoReprendre = this.add.text(cx, cy - 30, '[ REPRENDRE ]', {
+            fontSize: '22px',
+            fill: '#e94560',
+            fontStyle: 'bold'
+        }).setOrigin(0.5).setInteractive();
+
+        this.botoReiniciar = this.add.text(cx, cy + 40, '[ REINICIAR ]', {
+            fontSize: '22px',
+            fill: '#e94560',
+            fontStyle: 'bold'
+        }).setOrigin(0.5).setInteractive();
+
+        this.botoMenu = this.add.text(cx, cy + 110, '[ MENÚ PRINCIPAL ]', {
+            fontSize: '22px',
+            fill: '#e94560',
+            fontStyle: 'bold'
+        }).setOrigin(0.5).setInteractive();
+
+        this.configurarBotons();
+    }
+
+    /**
+     * Assigna els esdeveniments de clic i hover als tres botons.
+     * Pre: els tres botons han d'existir com a propietats de l'escena.
+     * Post: cada botó respon al clic i al ratolí per sobre.
+     */
+    configurarBotons() {
+        this.configurarHover(this.botoReprendre);
+        this.configurarHover(this.botoReiniciar);
+        this.configurarHover(this.botoMenu);
+
+        this.botoReprendre.on('pointerup', () => {
+            this.scene.resume('gameScene');
+            this.scene.stop('pauseScene');
+        });
+
+        this.botoReiniciar.on('pointerup', () => {
+            this.scene.stop('pauseScene');
+            this.scene.stop('gameScene');
+            this.scene.start('gameScene');
+        });
+
+        this.botoMenu.on('pointerup', () => {
+            this.scene.stop('pauseScene');
+            this.scene.stop('gameScene');
+            this.scene.start('MenuScene');
+        });
+    }
+
+    /**
+     * Aplica l'efecte hover estàndard a un botó de text.
+     * Pre: boto és un objecte Text de Phaser amb setStyle disponible.
+     * Post: el botó canvia de color en passar el ratolí per sobre.
+     */
+    configurarHover(boto) {
+        boto.on('pointerover', () => {
+            boto.setStyle({ fill: '#ffffff' });
+        });
+        boto.on('pointerout', () => {
+            boto.setStyle({ fill: '#e94560' });
+        });
+    }
+
+    update() { }
+}
